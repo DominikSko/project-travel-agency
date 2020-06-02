a9eb1f3099c1d80da0660eacdf55ec7c815e489d
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-06-02 13:13:16 +0200

    Adding order options, needs fix

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { Row, Col } from 'react-flexbox-grid';
-import OrderSummary from '../OrderSummary/OrderSummary.js';
 import PropTypes from 'prop-types';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = (props) => (
-  <Row>
-    <Col xs={12}>
-      <OrderSummary totalCost={props.tripCost} orderOptions={props.options}/>
-    </Col>
-  </Row>
-);
+class OrderForm extends React.Component {
 
-OrderForm.propTypes = {
-  tripCost: PropTypes.string,
-  options: PropTypes.object,
-};
+  static propTypes = {
+    tripCost: PropTypes.node,
+    options: PropTypes.object,
+    pricing: PropTypes.array,
+    setOrderOption: PropTypes.func,
+  }
+
+  render() {
+    const { tripCost, options, setOrderOption } = this.props;
+    return (
+      <Grid>
+        <Row>
+          {pricing.map(option =>
+            <Col md={4} key={option.id}>
+              <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
+            </Col>
+          )}
+          <Col xs={12}>
+            <OrderSummary tripCost={tripCost} options={options} />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
 
 export default OrderForm;
