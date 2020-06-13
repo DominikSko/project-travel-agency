f1e5f2e7abb80d21a58d5b8ee09a9caa6148dbdf
Dominik Skorupski <dominik.skorupski1@gmail.com>
2020-06-14 00:16:04 +0200

    Added trip names, tripID for API and need to fill contact forms

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
-import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
-import OrderOption from '../OrderOption/OrderOption';
-import Button from '../../common/Button/Button';
 
+import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
+
+import Button from '../../common/Button/Button';
 import {formatPrice} from '../../../utils/formatPrice';
 import {calculateTotal} from '../../../utils/calculateTotal';
 import settings from '../../../data/settings.js';
 
-const sendOrder = (options, tripCost) => {
+const sendOrder = (options, tripCost, tripName, tripId, tripCode) => {
   const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const {contact, name} = options;
+
+  if ( name === '' || name.length < 5 ) {
+    alert('Please fill name with 5 or more letters');
+    return;
+  }
+  if ( contact === '' || contact.length < 5 ) {
+    alert('Please fill contact with 5 or more letters');
+    return;
+  }
 
   const payload = {
     ...options,
     totalCost,
+    tripName,
+    tripId,
+    tripCode,
   };
 
   const url = settings.db.url + '/' + settings.db.endpoint.orders;
@@ -37,32 +52,27 @@ const sendOrder = (options, tripCost) => {
     });
 };
 
-class OrderForm extends React.Component {
+const OrderForm = ({ tripCost, options, setOrderOption, tripName, tripId, tripCode }) => (
+  <Row>
+    {pricing.map(option => (
+      <Col md={4} key={option.id}>
+        <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
+      </Col>
+    ))}
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+    </Col>
+    <Button onClick={() => sendOrder(options, tripCost, tripName, tripId, tripCode)}>Order now!</Button>
+  </Row>
+);
 
-  static propTypes = {
-    tripCost: PropTypes.node,
-    options: PropTypes.object,
-    setOrderOption: PropTypes.func,
-  }
-
-  render() {
-
-    const { tripCost, options, setOrderOption } = this.props;
-    return (
-      <Row>
-        {pricing.map(option =>
-          <Col md={4} key={option.id}>
-            <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption} />
-          </Col>
-        )}
-        <Col xs={12}>
-          <OrderSummary tripCost={tripCost} options={options} />
-        </Col>
-        <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
-      </Row>
-
-    );
-  }
-}
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+  tripId: PropTypes.string,
+  tripName: PropTypes.string,
+  tripCode: PropTypes.string,
+};
 
 export default OrderForm;
